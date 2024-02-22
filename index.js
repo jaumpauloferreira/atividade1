a7d3dc875cc595a79d73c5e94783b2609b8fab92
João Paulo Ferreira da Silva <137740174+jaumpauloferreira@users.noreply.github.com>
2024-02-22 18:37:51 -0300

    atualização

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 import express from 'express';
+import process from 'process';
+import path from 'path';
+import session from 'express-session';
+import autenticar from './seguranca/autenticar.js';
 
 const host='0.0.0.0';
 const porta = 3000;
+
 const app = express();
 
-app.get
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'M1nH4Ch4v3S3cr3t4',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {  
+        maxAge: 60 * 1000 * 15
+    }
+}))
+
+app.post('/login', (requisicao, resposta)=>{
+    const usuario = requisicao.body.usuario;
+    const senha   = requisicao.body.senha;
+    if (usuario && senha && usuario === 'Jaum' && senha === '123'){
+        requisicao.session.usuarioLogado = true;
+        resposta.redirect('/ticket.html');
+    }
+    else{
+        resposta.redirect('/login.html');
+    }
+})
+
+app.use(express.static(path.join(process.cwd(), 'publico')));
+app.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
+
 
 app.listen(porta, host, ()=>{
     console.log(`Servidor escutando em http://${host}:${porta}`);
