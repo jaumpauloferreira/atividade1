e0d63de4b501e5f20e6c5ddaca829144169c4f04
João Paulo Ferreira da Silva <137740174+jaumpauloferreira@users.noreply.github.com>
2024-03-04 21:24:34 -0300

    trabalho concluido

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import session from "express-session";
 import autenticar from "./segurança/autenticar.js";
 
 const host = "0.0.0.0";
-const porta = 30;
+const porta = 3000;
 
 const app = express();
 
