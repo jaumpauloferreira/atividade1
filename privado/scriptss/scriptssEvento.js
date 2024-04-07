86db295cbc641761e30cf2a8febd9ad7068fd39b
João Paulo Ferreira da Silva <137740174+jaumpauloferreira@users.noreply.github.com>
2024-04-07 00:01:41 -0300

    projeto quase finalizado

--- privado/scriptss/scriptssEvento.js
+++ privado/scriptss/scriptssEvento.js
@@ -1,8 +1,8 @@
 const formularioEvento = document.getElementById("formEvento");
-//cuidado para não chamar a função/ e sim atribuí-la ao método onsubmit
 formularioEvento.onsubmit = validarFormulario;
-window.onload = buscarEventos; //função não está sendo chamada e sim atribuída
-
+window.onload = buscarEventos;
+document.getElementById("excluir").onclick = excluirEvento;
+document.getElementById("atualizar").onclick = atualizarEvento;
 
 function validarFormulario(evento) {
   evento.preventDefault();
@@ -33,53 +33,38 @@ function validarFormulario(evento) {
   } else {
     formularioEvento.classList.add('was-validated');
   }
-  evento.preventDefault();
-  evento.stopPropagation();
 }
 
 function cadastrarEvento(evento) {
   fetch('http://localhost:3000/eventos', {
     method: "POST",
-    headers: {
-      "content-Type": "application/json",
-    },
+    headers: { 'content-Type': "application/json" },
     body: JSON.stringify(evento),
   })
-    .then((resposta) => {
-      return resposta.json();
-    })
+    .then((resposta) => resposta.json())
     .then((dados) => {
       if (dados.status) {
         mostrarMensagem(dados.mensagem, true);
-        buscarEventos(); //atualiza a tabela
-
-
+        buscarEventos();
       } else {
         mostrarMensagem(dados.mensagem, false);
       }
     })
-    .catch((erro) => {
-      mostrarMensagem(erro.message, false);
-    });
+    .catch((erro) => mostrarMensagem(erro.message, false));
 }
 
-function buscarEventos(){
-  fetch('http://localhost:3000/eventos', {method: 'GET'}).then((resposta)=>{
-    return resposta.json();
-  }).then((dados)=>{
-      if (Array.isArray(dados)){
-          exibirTabelaEventos(dados);
-      }
-      else{
+function buscarEventos() {
+  fetch('http://localhost:3000/eventos', { method: 'GET' })
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      if (Array.isArray(dados)) {
+        exibirTabelaEventos(dados);
+      } else {
         mostrarMensagem(dados.mensagem, false);
       }
-  })
-  .catch((erro)=>{
-        mostrarMensagem(erro.message, false);
-  });
+    })
+    .catch((erro) => mostrarMensagem(erro.message, false));
 }
-
-
 
 function mostrarMensagem(mensagem, sucesso = false) {
   const divMensagem = document.getElementById("mensagem");
@@ -103,7 +88,6 @@ function exibirTabelaEventos(listaEventos) {
   const espacoTabela = document.getElementById('espacoTabela');
   espacoTabela.innerHTML = '';
   if (listaEventos.length > 0) {
-    //constroi a tabela
     const tabela = document.createElement('table');
     tabela.className ='table table-striped table-hover';
     const cabecalho = document.createElement('thead');
@@ -137,7 +121,7 @@ function exibirTabelaEventos(listaEventos) {
                 <td>${evento.ingressos}</td>
                 <td>${evento.valor}</td>
                 <td>
-                    <button onclick = "selecionarEvento('${evento.codigo}',
+                    <button onclick="selecionarEvento('${evento.codigo}',
                                                          '${evento.nome}',
                                                          '${evento.email}',
                                                          '${evento.telefone}',
@@ -145,12 +129,11 @@ function exibirTabelaEventos(listaEventos) {
                                                          '${evento.cpf}',
                                                          '${evento.endereco}',
                                                          '${evento.ingressos}',
-                                                         '${evento.valor}',)">
+                                                         '${evento.valor}')">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
                     </svg>
-                
-                    </button>
+                </button>
              </td>`;
       corpo.appendChild(linha);
      }
@@ -171,63 +154,101 @@ function selecionarEvento(codigo, nome, email, telefone, rg, cpf, endereco, ingr
   document.getElementById('endereco').value = endereco;
   document.getElementById('ingressos').value = ingressos;
   document.getElementById('valor').value = valor;
-
 }
 
-function prepararFormulario(nome, email, telefone, rg, cpf, endereco, ingressos, valor, acao){
-  let botaoCadastrar = document.getElementById('cadastrar');
-  let botaoAtualizar = document.getElementById('atualizar');
-  let botaoExcluir = document.getElementById('excluir');
+function atualizarEvento() {
+  const codigo = document.getElementById('codigo').value;
+  if (confirm("Confirma atualização do Evento?")) {
+    const evento = obterEventoDoFormulario('atualizacao');
+    if (evento) {
+      fetch(`http://localhost:3000/eventos/${codigo}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(evento)
+      })
+      .then((resposta) => {
+        if (!resposta.ok) {
+          return resposta.text().then(text => {
+            throw new Error(`Erro na requisição: ${resposta.status} ${resposta.statusText} ${text}`);
+          });
+        }
+        return resposta.json();
+      })
+      .then((dados) => {
+        console.log(dados); // Verifique a resposta do servidor
+        if (dados && dados.mensagem) {
+          mostrarMensagem(dados.mensagem, true);
+        } else {
+          mostrarMensagem("Ocorreu um erro ao processar a solicitação.", false);
+        }
+        buscarEventos();
+      })
+      .catch((erro) => {
+        mostrarMensagem(erro.message, false);
+      });
+    } else {
+      mostrarMensagem("Favor, informar corretamente os dados do Evento!", false);
+    }
+  }
+}
 
-  document.getElementById('nome').value = nome;
-  document.getElementById('email').value = email;
-  document.getElementById('telefone').value = telefone;
-  document.getElementById('rg').value = rg;
-  document.getElementById('cpf').value = cpf;
-  document.getElementById('endereco').value = endereco;
-  document.getElementById('ingressos').value = ingressos;
-  document.getElementById('valor').value = valor;
+function excluirEvento() {
+  const codigo = document.getElementById('codigo').value;
+  if (confirm("Confirma exclusão do Evento?")) {
+    fetch(`http://localhost:3000/eventos/${codigo}`, {
+      method: 'DELETE'
+    })
+    .then((resposta) => {
+      if (!resposta.ok) {
+        return resposta.text().then(text => {
+          throw new Error(`Erro na requisição: ${resposta.status} ${resposta.statusText} ${text}`);
+        });
+      }
+      return resposta.json();
+    })
+    .then((dados) => {
+      console.log(dados); // Verifique a resposta do servidor
+      if (dados && dados.mensagem) {
+        mostrarMensagem(dados.mensagem, true);
+      } else {
+        mostrarMensagem("Ocorreu um erro ao processar a solicitação.", false);
+      }
+      buscarEventos();
+    })
+    .catch((erro) => {
+      mostrarMensagem(erro.message, false);
+    });
+  }
+}
 
+function obterEventoDoFormulario(tipoOperacao) {
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const telefone = document.getElementById('telefone').value;
+  const rg = document.getElementById('rg').value;
+  const cpf = document.getElementById('cpf').value;
+  const endereco = document.getElementById('endereco').value;
+  const ingressos = document.getElementById('ingressos').value;
+  const valor = document.getElementById('valor').value;
 
-  if (acao ==="exclusao"){
+  const evento = {
+    nome,
+    email,
+    telefone,
+    rg,
+    cpf,
+    endereco,
+    ingressos,
+    valor,
+  };
 
-      botaoCadastrar.disabled = true;
-      botaoAtualizar.disabled = true;
-      botaoExcluir.disabled = false;
+  // Verifica se a operação é para atualização
+  if (tipoOperacao === 'atualizacao') {
+    const codigo = document.getElementById('codigo').value;
+    evento.codigo = codigo;
   }
 
-else if (acao === "atualizacao"){
-
-     botaoCadastrar.disabled = true;
-     botaoAtualizar.disabled = false;
-     botaoExcluir.disabled = true;
-
+  return evento;
 }
-else { //ação cadastrar
-
-     botaoCadastrar.disabled = false;
-     botaoAtualizar.disabled = true;
-     botaoExcluir.disabled = true;
-
-}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////  validação ingressos   //////    valor   ////////////////
-document.getElementById("ingressos").addEventListener("input", function () {
-  var ticketQuantity = parseInt(this.value);
-  var ticketPrice = 10;
-  var totalPrice = ticketQuantity * ticketPrice;
-  document.getElementById("valor").value = totalPrice.toFixed(2);
-});
